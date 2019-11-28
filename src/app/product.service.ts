5d6efab98e7dc69d5c993233b7207658ba358c32
Tisighe Livinstone <tisighelivinstone1b@gmail.com>
2019-11-29 00:52:14 +0100

    my first commit

---
+++ src/app/product.service.ts
@@ -0,0 +1,20 @@
+import { Product } from './product';
+import { Injectable } from '@angular/core';
+import { Http, Response} from '@angular/http';
+import "rxjs/add/operator/map";
+import { Album } from './album';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private _productsUrl = '../assets/products.json';
+  private _albumUrl = '../assets/album.json';
+  constructor(private _http: Http) { }
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
+  }
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+  }
+}
